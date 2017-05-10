c79eedbd47d2d52cb8ace6a807e854867f77083f
Adalberto GONZALEZ <adalg08@gmail.com>
2017-05-10 16:29:38 +0200

    backend done

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,21 @@
+class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    if @restaurant.reviews.create(review_params)
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Reset database..."
+Restaurant.destroy_all
+restaurants = [
+  {name: "Maison Michel", address: "Quai de Bacalan" , phone_number: "06 04 94 94 82", category: "french"},
+  {name: "Pepone", address: "Près de la Place de la Bourse", phone_number: "05 10 48 28 38" , category: "italian"},
+  {name: "Terijaki", address: "Rue du Ha", phone_number: "05 00 38 48 28" , category: "japanese"},
+  {name: "Les bonnes frites", address: "Rue de Flandre" , phone_number: "05 49 28 38 49" , category: "belgian"},
+  {name: "Sun tee", address: "Avenue de Choisy" , phone_number: "05 19 49 18 19", category: "chinese"}
+]
+Restaurant.create(restaurants)
+puts "Seed done!"
